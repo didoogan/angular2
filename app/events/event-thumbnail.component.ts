a512ba77a76c47413de1b4c9105533fc2ec0931c
Dima Timoshenko <tzd0409@gmail.com>
2017-06-04 15:15:40 +0300

    template syntax

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -2,7 +2,16 @@ import {Component, Input, Output, EventEmitter} from "@angular/core";
 
 @Component({
     selector: 'tumbnail-cmp',
-    templateUrl: 'app/events/event-thumbnail.component.html'
+    templateUrl: 'app/events/event-thumbnail.component.html',
+    styles: [`
+        .green {color: greenyellow !important;}
+        .yellow {color: yellow !important;}
+        .italic {font-style: italic !important;}
+        .bold {font-weight: bold;}
+        .thumbnail { min-height: 270px; }
+        .pad-left { margin-left: 10px;}
+        .well div { color: #bbb;}
+    `]
 })
 export class ThumbnailEvent {
     @Input() event: any;
